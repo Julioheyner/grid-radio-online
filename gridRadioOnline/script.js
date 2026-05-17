a126c389d93b21ce70960d1b64129d28c87ae96f
Julio Gonzales <157603264+Julioheyner@users.noreply.github.com>
2026-05-17 08:43:15 -0400

    Update script.js

--- gridRadioOnline/script.js
+++ gridRadioOnline/script.js
@@ -2329,7 +2329,7 @@ shareModal.innerHTML = `
       </div>
       
        <div class="share-option x" data-share="x">
-        <i class="fab fa-x-twitter"></i>
+        <i class="fa-brands fa-x-twitter" style="color: rgb(116, 192, 252);"></i>
         <span>X</span>
       </div>
       
